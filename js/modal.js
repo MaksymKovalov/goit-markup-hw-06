f440debcaa334771e0f0497e82d4131e02a56e55
starlei <starlei13@gmail.com>
2025-04-02 22:12:21 +0300

    add project home 6

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,24 @@
-(() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-modal]'),
-  };
+(() => {  
+  const refs = {  
+    openModalBtn: document.querySelector("[data-modal-open]"),  
+    closeModalBtn: document.querySelector("[data-modal-close]"),  
+    modal: document.querySelector("[data-modal]"),  
+  };  
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) return;  
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
-})();
+  refs.openModalBtn.addEventListener("click", toggleModal);  
+  refs.closeModalBtn.addEventListener("click", toggleModal);  
+  refs.modal.addEventListener("click", handleBackdropClick);  
+
+  function toggleModal() {  
+    refs.modal.classList.toggle("is-hidden");  
+    document.body.classList.toggle("no-scroll");  
+  }  
+
+  function handleBackdropClick(event) {  
+    if (event.target === event.currentTarget) {  
+      toggleModal();  
+    }  
+  }  
+})();  
